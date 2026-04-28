3eb7a02ec7f4e6dc84e365a9f7907b223449bdc4
yunus <programmerhacker001@gmail.com>
2026-04-29 01:16:52 +0300

    update

--- food-res/src/App.jsx
+++ food-res/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/reviews" Component={Reviews}/>
         <Route path="/orders" Component={Orders}/>
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- food-res/src/components/footer/Footer.jsx
+++ food-res/src/components/footer/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+    <footer>
+      <div className="top-footer">
+        <div className="footer-logo">
+          <div>
+            <a href="https://resturant-food.onrender.com">
+              <img src="../../../public/images/shLogo.png" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-topList">
+          <ul className="main-topList">
+            <li>
+              <ul>
+                <li> الصفحة الرئيسية </li>
+                <li> أكل شرقي </li>
+                <li> أكل غربي </li>
+                <li> مقبلات </li>
+                <li> مشروبات </li>
+              </ul>
+            </li>
+            <li>
+              <ul>
+                <li> قوم ادبوووك ولاااك </li>
+                <li> انتوا فلتوا فلتوا </li>
+                <li> لأنك حريمي </li>
+                <li> آني حتى مشي ما امشي </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-bottomList"></div>
+      </div>
+      <div className="bottom-footer">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, quam.</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
