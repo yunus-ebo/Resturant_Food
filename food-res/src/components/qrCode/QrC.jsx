b9e22ee8d3c3c618392ec34cbead48647030d285
yunus <programmerhacker001@gmail.com>
2026-05-28 19:55:30 +0300

    update

--- food-res/src/components/qrCode/QrC.jsx
+++ food-res/src/components/qrCode/QrC.jsx
@@ -1,52 +1,46 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 const QrC = () => {
-  
-  const [barCode,setBarCode] = useState(null);
-  const [loading,setLoading] = useState(true);
-  const [error,setError] = useState("");
+  const [barCode, setBarCode] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
   useEffect(() => {
     getQrData();
-  },[])
+  }, []);
   const getQrData = async () => {
     try {
       const res = await fetch(`${import.meta.env.VITE_API_URI}/api/qr`);
       console.log(res);
       // coverting datas to json
       const data = await res.json();
-      setBarCode(data.barCode);
+      setBarCode(data.restaurantQr);
     } catch (error) {
       console.log(error);
       setError("failed to fetch Qr code");
-    } finally{
+    } finally {
       setLoading(false);
     }
-  }
-  // do special returns for error and loading
-  if(loading){
-    return <h1>loading...</h1>
   };
-  if(error){
-    return <h1>{error}</h1>
+  // do special returns for error and loading
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
   }
   return (
     <div>
       <h1>{barCode.name}</h1>
       <p>{barCode.websiteUrl}</p>
       <img src={barCode.qrCode} alt="" width="300" />
-      <a 
-      href={barCode.qrCode}
-      download="restaurant-qr.png"
-      >
-        <button>
-          generate Qr
-        </button>
+      <a href={barCode.qrCode} download="restaurant-qr.png">
+        <button>generate Qr</button>
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default QrC
+export default QrC;
 
 /*
 INFO:
