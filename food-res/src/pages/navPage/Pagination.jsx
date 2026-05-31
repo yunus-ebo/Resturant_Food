24221e3570382eec505227384131a0fd5650c9d4
yunus <programmerhacker001@gmail.com>
2026-05-31 04:38:02 +0300

    update

--- food-res/src/pages/navPage/Pagination.jsx
+++ food-res/src/pages/navPage/Pagination.jsx
@@ -1,29 +1,35 @@
-
-const Pagination = ({pages, currentPag, setCurrentPag}) => {
-    const pagesNum = [];
-    for(let i = 1;i <= pages;i++){
-        pagesNum.push(i);
-    }
+const Pagination = ({ pages, currentPag, setCurrentPag }) => {
+  const pagesNum = [];
+  for (let i = 1; i <= pages; i++) {
+    pagesNum.push(i);
+  }
 
   return (
     <div className="paginationContainer">
-        <button
-        onClick={() => setCurrentPag(prev => prev + 1)}
+      <button
+        onClick={() => setCurrentPag((prev) => prev + 1)}
         disabled={currentPag === pages}
-        className="nextBtn"> التالي </button>
-            {pagesNum.map(item => (
-                <div
-                onClick={() => setCurrentPag(item)}
-                 className="paginate_perPage">
-                    {item}
-                </div>
-            ))}
-        <button
-        onClick={() => setCurrentPag(prev => prev - 1)}
+        className="nextBtn"
+      >
+        التالي
+      </button>
+      {pagesNum.map((item) => (
+        <div
+          onClick={() => setCurrentPag(item)}
+          className={currentPag === item ? "paginate_perPage pag-active" : "paginate_perPage"}
+        >
+          {item}
+        </div>
+      ))}
+      <button
+        onClick={() => setCurrentPag((prev) => prev - 1)}
         disabled={currentPag === 1}
-        className="previousBtn"> السابق </button>
+        className="previousBtn"
+      >
+        السابق
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
