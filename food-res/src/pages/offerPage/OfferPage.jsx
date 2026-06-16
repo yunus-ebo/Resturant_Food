ca251c5c02efc783408be420b1ea034cde1648ae
yunus <programmerhacker001@gmail.com>
2026-06-16 05:37:05 +0300

    update

--- food-res/src/pages/offerPage/OfferPage.jsx
+++ food-res/src/pages/offerPage/OfferPage.jsx
@@ -1,13 +1,39 @@
-import { useEffect } from "react"
-
+import "./offerPage.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchProductById } from "../../redux/apiCalls/productApiCall";
+import { imageUrI } from "../../imageUrI";
 
 const OfferPage = () => {
-  useEffect(() => {
-    window.scrollTo(0,0)
-  },[])
-  return (
-    <div>OfferPage</div>
-  )
-}
+  const { id } = useParams();
+  const { productItem } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
 
-export default OfferPage
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    dispatch(fetchProductById(id));
+  }, []);
+  return (
+    <div className="offerPageContainer">
+      <div className="offerPage-img">
+        <img src={imageUrI(productItem?.image)} alt="" />
+      </div>
+      <div className="offerPage-texts">
+        <h2>{productItem?.description} :</h2>
+        <ul className="offerPage-ul">
+          {productItem?.descriptions.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="offerPage-cart">
+        <span> أضف لسلة طلباتك </span>
+        <i class="fa-solid fa-cart-plus cart_plus"></i>
+      </div>
+    </div>
+  );
+};
+
+export default OfferPage;
